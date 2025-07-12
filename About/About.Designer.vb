d1dc6bf3fab3e25977b4f415c837ceb44a01590d
Jim Dale Abad <147327072+JimAbad@users.noreply.github.com>
2025-07-13 04:46:51 +0800

    Add files via upload

---
+++ About/About.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class About
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(About))
+        PictureBox1 = New PictureBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        PictureBox2 = New PictureBox()
+        Label1 = New Label()
+        PictureBox3 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Gainsboro
+        PictureBox1.Location = New Point(0, 0)
+        PictureBox1.Margin = New Padding(2)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(1099, 58)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("Arial", 11F)
+        Label2.ForeColor = Color.WhiteSmoke
+        Label2.Location = New Point(181, 304)
+        Label2.Margin = New Padding(2, 0, 2, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(706, 73)
+        Label2.TabIndex = 3
+        Label2.Text = "Welcome to McKings! We serve delicious fast food made with fresh ingredients. Our menu includes tasty burgers, crispy fries, salads, and desserts, all aimed at satisfying your cravings quickly. "
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label3
+        ' 
+        Label3.Font = New Font("Arial", 11F)
+        Label3.ForeColor = Color.WhiteSmoke
+        Label3.Location = New Point(181, 403)
+        Label3.Margin = New Padding(2, 0, 2, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(715, 52)
+        Label3.TabIndex = 4
+        Label3.Text = "We’re committed to great customer service and supporting our local community. Stop by McKings for a meal you’ll love!"
+        Label3.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.Gainsboro
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.Font = New Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.ForeColor = SystemColors.ActiveCaptionText
+        Button2.Location = New Point(96, 12)
+        Button2.Margin = New Padding(2)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(127, 36)
+        Button2.TabIndex = 6
+        Button2.Text = "ABOUT US"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Gainsboro
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.Font = New Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.ForeColor = SystemColors.ActiveCaptionText
+        Button3.Location = New Point(237, 12)
+        Button3.Margin = New Padding(2)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(154, 36)
+        Button3.TabIndex = 7
+        Button3.Text = "CONTACT US"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.ForestGreen
+        Button4.FlatStyle = FlatStyle.Flat
+        Button4.Font = New Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button4.ForeColor = SystemColors.ButtonFace
+        Button4.Location = New Point(846, 18)
+        Button4.Margin = New Padding(2)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(90, 27)
+        Button4.TabIndex = 8
+        Button4.Text = "LOG IN"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.OrangeRed
+        Button5.FlatStyle = FlatStyle.Flat
+        Button5.Font = New Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button5.ForeColor = SystemColors.ButtonFace
+        Button5.Location = New Point(949, 18)
+        Button5.Margin = New Padding(2)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(107, 27)
+        Button5.TabIndex = 9
+        Button5.Text = "REGISTER"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(-1, 57)
+        PictureBox2.Margin = New Padding(2)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(1099, 178)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 10
+        PictureBox2.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.FromArgb(CByte(183), CByte(28), CByte(28))
+        Label1.FlatStyle = FlatStyle.Flat
+        Label1.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.WhiteSmoke
+        Label1.Location = New Point(464, 221)
+        Label1.Margin = New Padding(2, 0, 2, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(161, 40)
+        Label1.TabIndex = 11
+        Label1.Text = "About us"
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(0, 0)
+        PictureBox3.Margin = New Padding(2)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(81, 58)
+        PictureBox3.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox3.TabIndex = 14
+        PictureBox3.TabStop = False
+        ' 
+        ' About
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(183), CByte(28), CByte(28))
+        ClientSize = New Size(1096, 519)
+        Controls.Add(PictureBox3)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox2)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(PictureBox1)
+        Margin = New Padding(2)
+        Name = "About"
+        Text = "About us"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+
+End Class
