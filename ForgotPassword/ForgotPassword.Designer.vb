3dcfe48505ac3459bfd478af577468f68a7fcc7b
Jim Dale Abad <147327072+JimAbad@users.noreply.github.com>
2025-07-14 02:18:26 +0800

    Add files via upload

---
+++ ForgotPassword/ForgotPassword.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ForgotPassword
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ForgotPassword))
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        Button1 = New Button()
+        LinkLabel1 = New LinkLabel()
+        PictureBox3 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(10, -1)
+        PictureBox1.Margin = New Padding(2)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(106, 94)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.WhiteSmoke
+        PictureBox2.Location = New Point(390, 80)
+        PictureBox2.Margin = New Padding(2)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(328, 373)
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.WhiteSmoke
+        Label1.Font = New Font("Arial", 11F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = SystemColors.ControlDarkDark
+        Label1.Location = New Point(449, 121)
+        Label1.Margin = New Padding(2, 0, 2, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(208, 22)
+        Label1.TabIndex = 2
+        Label1.Text = "FORGOT PASSWORD"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.WhiteSmoke
+        Label2.Font = New Font("Arial", 7F)
+        Label2.Location = New Point(442, 171)
+        Label2.Margin = New Padding(2, 0, 2, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(66, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Username"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.WhiteSmoke
+        Label3.Font = New Font("Arial", 7F)
+        Label3.Location = New Point(442, 231)
+        Label3.Margin = New Padding(2, 0, 2, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(90, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "New password"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.WhiteSmoke
+        Label4.Font = New Font("Arial", 7F)
+        Label4.Location = New Point(442, 291)
+        Label4.Margin = New Padding(2, 0, 2, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(135, 15)
+        Label4.TabIndex = 5
+        Label4.Text = "Confirm new password"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(442, 186)
+        TextBox1.Margin = New Padding(2)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(208, 27)
+        TextBox1.TabIndex = 6
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(442, 246)
+        TextBox2.Margin = New Padding(2)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(208, 27)
+        TextBox2.TabIndex = 7
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(442, 306)
+        TextBox3.Margin = New Padding(2)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(208, 27)
+        TextBox3.TabIndex = 8
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.BackColor = Color.Gainsboro
+        CheckBox1.Location = New Point(657, 250)
+        CheckBox1.Margin = New Padding(2)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(18, 17)
+        CheckBox1.TabIndex = 9
+        CheckBox1.UseVisualStyleBackColor = False
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.BackColor = Color.Gainsboro
+        CheckBox2.Location = New Point(657, 311)
+        CheckBox2.Margin = New Padding(2)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(18, 17)
+        CheckBox2.TabIndex = 10
+        CheckBox2.UseVisualStyleBackColor = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.SlateBlue
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Font = New Font("Arial", 6F)
+        Button1.ForeColor = Color.Gainsboro
+        Button1.Location = New Point(442, 346)
+        Button1.Margin = New Padding(2)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(106, 33)
+        Button1.TabIndex = 11
+        Button1.Text = "Change Password"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.BackColor = Color.WhiteSmoke
+        LinkLabel1.Font = New Font("Arial", 6F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LinkLabel1.Location = New Point(473, 398)
+        LinkLabel1.Margin = New Padding(2, 0, 2, 0)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(71, 13)
+        LinkLabel1.TabIndex = 12
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Back to Login"
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackColor = Color.WhiteSmoke
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(445, 398)
+        PictureBox3.Margin = New Padding(2)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(23, 12)
+        PictureBox3.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox3.TabIndex = 13
+        PictureBox3.TabStop = False
+        ' 
+        ' ForgotPassword
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(183), CByte(28), CByte(28))
+        ClientSize = New Size(1096, 519)
+        Controls.Add(PictureBox3)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Button1)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Margin = New Padding(2)
+        Name = "ForgotPassword"
+        Text = "Forgot Password"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents PictureBox3 As PictureBox
+
+End Class
