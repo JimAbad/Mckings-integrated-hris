927f2c62708061fbfe630a199d3168db09f4ff46
Jim Dale Abad <147327072+JimAbad@users.noreply.github.com>
2025-07-20 02:41:13 +0800

    Add files via upload

---
+++ PrivacyPolicy/PrivacyPolicy.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class PrivacyPolicy
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PrivacyPolicy))
+        Label1 = New Label()
+        Label2 = New Label()
+        LinkLabel1 = New LinkLabel()
+        PictureBox1 = New PictureBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        Label17 = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label1.Location = New Point(34, 42)
+        Label1.Margin = New Padding(2, 0, 2, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(155, 26)
+        Label1.TabIndex = 0
+        Label1.Text = "Privacy Policy"
+        ' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("Arial", 9F)
+        Label2.Location = New Point(34, 66)
+        Label2.Margin = New Padding(2, 0, 2, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(910, 41)
+        Label2.TabIndex = 1
+        Label2.Text = "We value your privacy and are committed to protecting your personal information. This privacy policy outlines how we collect, use, and safeguard the data you provide while using our system."
+        Label2.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Font = New Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LinkLabel1.Location = New Point(72, 12)
+        LinkLabel1.Margin = New Padding(2, 0, 2, 0)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(147, 19)
+        LinkLabel1.TabIndex = 10
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Back to Homepage"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(34, 12)
+        PictureBox1.Margin = New Padding(2, 2, 2, 2)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(33, 22)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 11
+        PictureBox1.TabStop = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label3.Location = New Point(34, 114)
+        Label3.Margin = New Padding(2, 0, 2, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(242, 26)
+        Label3.TabIndex = 12
+        Label3.Text = "Information We Collect"
+        ' 
+        ' Label4
+        ' 
+        Label4.Font = New Font("Arial", 9F)
+        Label4.Location = New Point(34, 137)
+        Label4.Margin = New Padding(2, 0, 2, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(1032, 26)
+        Label4.TabIndex = 13
+        Label4.Text = "• Personal Data: This includes your name, contact information, employee ID, and other related information."
+        Label4.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label5
+        ' 
+        Label5.Font = New Font("Arial", 9F)
+        Label5.Location = New Point(34, 161)
+        Label5.Margin = New Padding(2, 0, 2, 0)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(1032, 26)
+        Label5.TabIndex = 14
+        Label5.Text = "• Usage Data: Information on how you interact with the system, such as features accessed and time spent on the platform."
+        Label5.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label6
+        ' 
+        Label6.Font = New Font("Arial", 9F)
+        Label6.Location = New Point(34, 186)
+        Label6.Margin = New Padding(2, 0, 2, 0)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(1032, 26)
+        Label6.TabIndex = 15
+        Label6.Text = "• Technical Data: IP addresses, browser types, and device information used to access the system."
+        Label6.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label7.Location = New Point(37, 217)
+        Label7.Margin = New Padding(2, 0, 2, 0)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(319, 26)
+        Label7.TabIndex = 16
+        Label7.Text = "How We Use Your Information"
+        ' 
+        ' Label8
+        ' 
+        Label8.Font = New Font("Arial", 9F)
+        Label8.Location = New Point(34, 242)
+        Label8.Margin = New Padding(2, 0, 2, 0)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(1032, 26)
+        Label8.TabIndex = 17
+        Label8.Text = "• Processing payroll and managing employee records."
+        Label8.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label9
+        ' 
+        Label9.Font = New Font("Arial", 9F)
+        Label9.Location = New Point(34, 265)
+        Label9.Margin = New Padding(2, 0, 2, 0)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(1032, 26)
+        Label9.TabIndex = 18
+        Label9.Text = "• Tracking attendance and leave requests."
+        Label9.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label10
+        ' 
+        Label10.Font = New Font("Arial", 9F)
+        Label10.Location = New Point(34, 288)
+        Label10.Margin = New Padding(2, 0, 2, 0)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(1032, 26)
+        Label10.TabIndex = 19
+        Label10.Text = "• Generating reports to improve operational efficiency."
+        Label10.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label11
+        ' 
+        Label11.Font = New Font("Arial", 9F)
+        Label11.Location = New Point(34, 313)
+        Label11.Margin = New Padding(2, 0, 2, 0)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(1032, 26)
+        Label11.TabIndex = 20
+        Label11.Text = "• Enhancing user experience by optimizing the system’s performance."
+        Label11.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label12.Location = New Point(34, 343)
+        Label12.Margin = New Padding(2, 0, 2, 0)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(199, 26)
+        Label12.TabIndex = 21
+        Label12.Text = "Security Measures"
+        ' 
+        ' Label13
+        ' 
+        Label13.Font = New Font("Arial", 9F)
+        Label13.Location = New Point(37, 370)
+        Label13.Margin = New Padding(2, 0, 2, 0)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(880, 36)
+        Label13.TabIndex = 22
+        Label13.Text = "We implement robust security measures to protect your data, including encryption, user access controls, and regular system audits to prevent unauthorized access."
+        Label13.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label14.Location = New Point(34, 412)
+        Label14.Margin = New Padding(2, 0, 2, 0)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(213, 26)
+        Label14.TabIndex = 23
+        Label14.Text = "Contact Information"
+        ' 
+        ' Label15
+        ' 
+        Label15.Font = New Font("Arial", 9F)
+        Label15.Location = New Point(34, 432)
+        Label15.Margin = New Padding(2, 0, 2, 0)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(1032, 26)
+        Label15.TabIndex = 24
+        Label15.Text = "For any privacy-related inquiries, please contact us at:"
+        Label15.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label16
+        ' 
+        Label16.Font = New Font("Arial", 9F)
+        Label16.Location = New Point(34, 457)
+        Label16.Margin = New Padding(2, 0, 2, 0)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(1032, 26)
+        Label16.TabIndex = 25
+        Label16.Text = "• Email: Mckingsph@gmail.com"
+        Label16.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label17
+        ' 
+        Label17.Font = New Font("Arial", 9F)
+        Label17.Location = New Point(34, 481)
+        Label17.Margin = New Padding(2, 0, 2, 0)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(1032, 26)
+        Label17.TabIndex = 26
+        Label17.Text = "• Phone: 09566496301 / 09972108716"
+        Label17.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' PrivacyPolicy
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Gainsboro
+        ClientSize = New Size(1096, 519)
+        Controls.Add(Label17)
+        Controls.Add(Label16)
+        Controls.Add(Label15)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(PictureBox1)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Margin = New Padding(2, 2, 2, 2)
+        Name = "PrivacyPolicy"
+        Text = "Privacy Policy"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+
+End Class
