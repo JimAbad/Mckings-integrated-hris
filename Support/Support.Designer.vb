f033dfcc4b8e1b4039ab3af6f8bf8696192f840d
Jim Dale Abad <147327072+JimAbad@users.noreply.github.com>
2025-08-02 05:12:45 +0800

    Add files via upload

---
+++ Support/Support.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Support
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Support))
+        PictureBox1 = New PictureBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Label5 = New Label()
+        RichTextBox1 = New RichTextBox()
+        Label6 = New Label()
+        PictureBox2 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Gainsboro
+        PictureBox1.Location = New Point(0, 0)
+        PictureBox1.Margin = New Padding(2)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(1099, 58)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.Gainsboro
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.Font = New Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(96, 12)
+        Button2.Margin = New Padding(2)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(127, 36)
+        Button2.TabIndex = 2
+        Button2.Text = "ABOUT US"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Gainsboro
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.Font = New Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(237, 12)
+        Button3.Margin = New Padding(2)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(154, 36)
+        Button3.TabIndex = 3
+        Button3.Text = "CONTACT US"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.ForestGreen
+        Button4.FlatStyle = FlatStyle.Flat
+        Button4.Font = New Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button4.ForeColor = Color.Gainsboro
+        Button4.Location = New Point(846, 18)
+        Button4.Margin = New Padding(2)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(90, 27)
+        Button4.TabIndex = 4
+        Button4.Text = "LOG IN"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.OrangeRed
+        Button5.FlatStyle = FlatStyle.Flat
+        Button5.Font = New Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button5.ForeColor = Color.Gainsboro
+        Button5.ImageAlign = ContentAlignment.MiddleRight
+        Button5.Location = New Point(949, 18)
+        Button5.Margin = New Padding(2)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(107, 27)
+        Button5.TabIndex = 5
+        Button5.Text = "REGISTER"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Comic Sans MS", 14F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.ForeColor = Color.WhiteSmoke
+        Label5.Location = New Point(184, 423)
+        Label5.Margin = New Padding(2, 0, 2, 0)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(132, 35)
+        Label5.TabIndex = 10
+        Label5.Text = "Feedback:"
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(314, 423)
+        RichTextBox1.Margin = New Padding(2)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(462, 87)
+        RichTextBox1.TabIndex = 11
+        RichTextBox1.Text = ""
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Arial", 26F)
+        Label6.ForeColor = Color.WhiteSmoke
+        Label6.Location = New Point(839, 58)
+        Label6.Margin = New Padding(2, 0, 2, 0)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(230, 50)
+        Label6.TabIndex = 16
+        Label6.Text = "Contact us"
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(-1, 58)
+        PictureBox2.Margin = New Padding(2)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(1099, 362)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 17
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(-1, 0)
+        PictureBox3.Margin = New Padding(2)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(81, 58)
+        PictureBox3.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox3.TabIndex = 18
+        PictureBox3.TabStop = False
+        ' 
+        ' Support
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Firebrick
+        ClientSize = New Size(1096, 519)
+        Controls.Add(PictureBox3)
+        Controls.Add(Label6)
+        Controls.Add(RichTextBox1)
+        Controls.Add(Label5)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(PictureBox1)
+        Controls.Add(PictureBox2)
+        Margin = New Padding(2)
+        Name = "Support"
+        Text = "Support/Contact"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+
+End Class
