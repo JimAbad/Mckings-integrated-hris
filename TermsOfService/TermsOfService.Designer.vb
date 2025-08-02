5fc8f4a2b1c3040298c5e27f9025fb5ba396cab5
Jim Dale Abad <147327072+JimAbad@users.noreply.github.com>
2025-08-03 04:04:49 +0800

    Add files via upload

---
+++ TermsOfService/TermsOfService.Designer.vb
@@ -0,0 +1,297 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class TermsOfService
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TermsOfService))
+        LinkLabel1 = New LinkLabel()
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Font = New Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LinkLabel1.Location = New Point(85, 18)
+        LinkLabel1.Margin = New Padding(2, 0, 2, 0)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(147, 19)
+        LinkLabel1.TabIndex = 11
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Back to Homepage"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(47, 15)
+        PictureBox1.Margin = New Padding(2, 2, 2, 2)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(33, 22)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 12
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label1.Location = New Point(47, 51)
+        Label1.Margin = New Padding(2, 0, 2, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(228, 26)
+        Label1.TabIndex = 13
+        Label1.Text = "Acceptance of Terms"
+        ' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("Arial", 9F)
+        Label2.Location = New Point(47, 75)
+        Label2.Margin = New Padding(2, 0, 2, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(1032, 25)
+        Label2.TabIndex = 14
+        Label2.Text = "By using this system, you agree to comply with these terms. If you do not agree, please refrain from using the system." & vbLf & vbLf
+        Label2.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label3.Location = New Point(47, 106)
+        Label3.Margin = New Padding(2, 0, 2, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(226, 26)
+        Label3.TabIndex = 15
+        Label3.Text = "User Responsibilities"
+        ' 
+        ' Label4
+        ' 
+        Label4.Font = New Font("Arial", 9F)
+        Label4.Location = New Point(47, 130)
+        Label4.Margin = New Padding(2, 0, 2, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(1032, 28)
+        Label4.TabIndex = 16
+        Label4.Text = "• Users must provide accurate and up-to-date information."
+        Label4.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label5
+        ' 
+        Label5.Font = New Font("Arial", 9F)
+        Label5.Location = New Point(47, 151)
+        Label5.Margin = New Padding(2, 0, 2, 0)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(1032, 28)
+        Label5.TabIndex = 17
+        Label5.Text = "• Users are responsible for maintaining the confidentiality of their account credentials."
+        Label5.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label6
+        ' 
+        Label6.Font = New Font("Arial", 9F)
+        Label6.Location = New Point(47, 172)
+        Label6.Margin = New Padding(2, 0, 2, 0)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(1032, 28)
+        Label6.TabIndex = 18
+        Label6.Text = "• Misuse of the system, such as unauthorized access or tampering, is prohibited."
+        Label6.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label7.Location = New Point(47, 207)
+        Label7.Margin = New Padding(2, 0, 2, 0)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(141, 26)
+        Label7.TabIndex = 19
+        Label7.Text = "Data Privacy"
+        ' 
+        ' Label8
+        ' 
+        Label8.Font = New Font("Arial", 9F)
+        Label8.Location = New Point(48, 231)
+        Label8.Margin = New Padding(2, 0, 2, 0)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(841, 36)
+        Label8.TabIndex = 20
+        Label8.Text = "Your personal information is handled according to our Privacy Policy. By using this system, you agree to our data collection and usage practices as outlined in that policy."
+        Label8.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label9.Location = New Point(47, 278)
+        Label9.Margin = New Padding(2, 0, 2, 0)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(231, 26)
+        Label9.TabIndex = 21
+        Label9.Text = "Limitations of Liability"
+        ' 
+        ' Label10
+        ' 
+        Label10.Font = New Font("Arial", 9F)
+        Label10.Location = New Point(48, 303)
+        Label10.Margin = New Padding(2, 0, 2, 0)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(841, 36)
+        Label10.TabIndex = 22
+        Label10.Text = "We are not liable for any direct, indirect, or consequential damages arising from the use of this system, including data loss or unauthorized access."
+        Label10.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label11.Location = New Point(47, 346)
+        Label11.Margin = New Padding(2, 0, 2, 0)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(238, 26)
+        Label11.TabIndex = 23
+        Label11.Text = "Termination of Access"
+        ' 
+        ' Label12
+        ' 
+        Label12.Font = New Font("Arial", 9F)
+        Label12.Location = New Point(47, 370)
+        Label12.Margin = New Padding(2, 0, 2, 0)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(841, 26)
+        Label12.TabIndex = 24
+        Label12.Text = "We reserve the right to suspend or terminate your access to the system if you violate these terms or engage in unlawful activity."
+        Label12.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label13.Location = New Point(47, 403)
+        Label13.Margin = New Padding(2, 0, 2, 0)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(199, 26)
+        Label13.TabIndex = 25
+        Label13.Text = "Changes to Terms"
+        ' 
+        ' Label14
+        ' 
+        Label14.Font = New Font("Arial", 9F)
+        Label14.Location = New Point(48, 427)
+        Label14.Margin = New Padding(2, 0, 2, 0)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(841, 26)
+        Label14.TabIndex = 26
+        Label14.Text = "We may update these terms from time to time. Continued use of the system implies acceptance of any changes."
+        Label14.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Font = New Font("Arial", 13F, FontStyle.Bold)
+        Label15.Location = New Point(48, 457)
+        Label15.Margin = New Padding(2, 0, 2, 0)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(213, 26)
+        Label15.TabIndex = 27
+        Label15.Text = "Contact Information"
+        ' 
+        ' Label16
+        ' 
+        Label16.Font = New Font("Arial", 9F)
+        Label16.Location = New Point(48, 481)
+        Label16.Margin = New Padding(2, 0, 2, 0)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(841, 26)
+        Label16.TabIndex = 28
+        Label16.Text = "For questions or concerns about these terms, please contact us at Mckingsph@gmail.com"
+        Label16.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' TermsOfService
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Gainsboro
+        ClientSize = New Size(1096, 519)
+        Controls.Add(Label16)
+        Controls.Add(Label15)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Controls.Add(LinkLabel1)
+        Margin = New Padding(2, 2, 2, 2)
+        Name = "TermsOfService"
+        Text = "Terms of Service"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+
+End Class
